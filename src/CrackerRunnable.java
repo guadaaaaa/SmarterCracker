970058e64d418589de2536f76e4287f231e55597
LAB14_USER ON WS33 <L14Y15W33@citulab.net>
2024-03-19 18:06:54 +0800

    drained

--- src/CrackerRunnable.java
+++ src/CrackerRunnable.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class CrackerRunnable {
+public class CrackerRunnable implements Runnable{
+
+    public static boolean found = false;
+    public static String password;
+    private final int vowelPos;
+    private final char vowel;
+    public CrackerRunnable(char vowel, int vowel_position){
+        this.vowel = vowel;
+        this.vowelPos = vowel_position;
+    }
+    private String check(String atk){
+        return atk.substring(0, vowelPos) + vowel + atk.substring(vowelPos);
+    }
+    public void run(){
+        int len = password.length(); //
+        String atk = "a".repeat(len-1);
+        while (!found && !check(atk).equals(password)) {
+            System.out.println(Thread.currentThread().getName() + ": "+ check(atk));
+            int i;
+            for (i = atk.length()-1; i >= 0 && atk.charAt(i) == 'z'; i--);
+            if (i < 0) return;
+            String first = atk.substring(0, i);
+            char next = (char) (atk.charAt(i) + 1);
+            String after = "a".repeat(len - i - 2);
+            atk = first + next + after;
+        }
+        if (check(atk).equals(password)) {
+            found = true;
+            System.out.println(Thread.currentThread().getName() + " found: " + check(atk));
+        }
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static final char[] VOWELS = {'a','e','i','o','u'};
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter password: ");
+        CrackerRunnable.password = sc.nextLine();
+        int thrdCount = 1;
+        ArrayList<Thread> checkerThreads = new ArrayList<>();
+        for(int i = 0; i< CrackerRunnable.password.length(); i++){
+            for(int j=0; j<5; j++){
+                CrackerRunnable c = new CrackerRunnable(VOWELS[j],i);
+                Thread t = new Thread(c);
+                if(thrdCount > 9){
+                    t.setName("Thread "+ thrdCount);
+                } else {
+                    t.setName("Thread 0"+ thrdCount);
+                }
+                thrdCount++;
+                checkerThreads.add(t);
+                t.start();
+            }
+        }
+        for( Thread t : checkerThreads){
+            try {
+                t.join();
+            } catch (InterruptedException e) {}
+        }
     }
+
 }
